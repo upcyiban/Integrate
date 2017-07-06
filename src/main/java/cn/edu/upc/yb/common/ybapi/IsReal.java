9ca2895eac3dc395ce7668511950d20826494a27
LAPTOP-KHA5E3F0\idea <2286824491@qq.com>
2017-07-06 22:36:26 +0800

    添加user接口

---
+++ src/main/java/cn/edu/upc/yb/common/ybapi/IsReal.java
@@ -0,0 +1,63 @@
+package cn.edu.upc.yb.common.ybapi;
+
+import cn.edu.upc.yb.common.dto.ErrorReporter;
+import cn.edu.upc.yb.common.security.service.JwtTokenUtil;
+import cn.edu.upc.yb.common.service.QueryService;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+/**
+ * Created by idea on 2017/7/5.
+ */
+@Service
+public class IsReal {
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private QueryService queryService;
+
+    /**
+     * 易班https://openapi.yiban.cn/user/me接口封装
+     *
+     * @param token upcyiban token
+     * @return
+     */
+
+    public Object  getIsReal(String  token ) throws IOException {
+
+        String ybtoken = jwtTokenUtil.getYbaccessToken(token);
+        String yb_userid = jwtTokenUtil.getYBidFromTocken(token);
+        String queryString = "access_token=" + ybtoken + "&yb_userid=" + yb_userid;
+        String result = queryService.getYbApi("user/is_real", queryString);
+        Gson gson = new Gson();
+        System.out.println(result);
+
+        try {
+        Check check    = gson.fromJson(result,Check.class);
+            return check;
+        }catch (Exception e){
+            return new ErrorReporter(1,"请求失败");
+        }
+
+
+
+    }
+
+    class Check {
+        String  status;
+        String info;
+    }
+/*
+{
+  "status":"success",
+  "info":"验证结果"
+}
+//返回状态说明：true-是、false-否
+ */
+
+}
---
+++ src/main/java/cn/edu/upc/yb/common/ybapi/UserOther.java
@@ -0,0 +1,71 @@
+package cn.edu.upc.yb.common.ybapi;
+
+import cn.edu.upc.yb.common.dto.ErrorReporter;
+import cn.edu.upc.yb.common.security.service.JwtTokenUtil;
+import cn.edu.upc.yb.common.service.QueryService;
+import com.google.gson.Gson;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+/**
+ * Created by idea on 2017/7/2.
+ */
+@Service
+public class UserOther {
+  private final Log logger = LogFactory.getLog(this.getClass());
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private QueryService queryService;
+
+    /**
+     * 易班https://openapi.yiban.cn/user/me接口封装
+     *
+     * @param token upcyiban token
+     * @return
+     */
+    public Object getUserOther(String  token)  throws IOException{
+        String ybtoken = jwtTokenUtil.getYbaccessToken(token);
+        String ybid = jwtTokenUtil.getYBidFromTocken(token);
+        String queryString = "access_token=" + ybtoken + "&yb_userid=" + ybid;
+
+        String result = queryService.getYbApi("user/other", queryString);
+        Gson gson = new Gson();
+        logger.info(result);
+
+        try{
+            UserOtherInfo userOtherInfo = gson.fromJson(result,UserOther.UserOtherInfo.class);
+            return userOtherInfo;
+        } catch (Exception e){
+            return new ErrorReporter(1,"请求失败");
+        }
+
+    }
+
+    class UserOtherInfo {
+
+        public  String  stauts;
+
+        public class  info {
+
+           String yb_userid;
+           String yb_username;
+           String yb_usernick;
+           String yb_sex;
+           String yb_userhead;
+
+           String yb_schoolid;
+           String yb_schoolname;
+
+
+        }
+
+
+    }
+}
