e1edf6b50aab5ba11f29df4920f80c189a86e023
liliangbin <2286824491@qq.com>
2018-03-14 23:11:25 +0800

    添加API数据。。。

--- src/main/java/cn/edu/upc/yb/lottery/controller/LotteryController.java
+++ src/main/java/cn/edu/upc/yb/lottery/controller/LotteryController.java
@@ -1,6 +1,11 @@
 package cn.edu.upc.yb.lottery.controller;
 
+import cn.edu.upc.yb.common.dto.SwaggerParameter;
 import cn.edu.upc.yb.lottery.service.LotteryService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.models.Swagger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +26,10 @@ public class LotteryController {
     private LotteryService lotteryService;
 
     @GetMapping("")
+    @ApiOperation(value = "", notes = "获取抽奖的列表")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = SwaggerParameter.Authorization, value = "token", dataType = "String")
+    })
     public Object lotteryIndex(HttpServletRequest request) {
 
         return lotteryService.getLotteryList();
@@ -28,40 +37,72 @@ public class LotteryController {
 
     @PostMapping("/userInfo")
     @ResponseBody
-    public Object userInfo(HttpServletRequest request) throws IOException{
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = SwaggerParameter.Authorization, value = "token", dataType = "String")
+    })
+
+    public Object userInfo(HttpServletRequest request) throws IOException {
 
         return lotteryService.getUserInfo(request);
     }
 
     @PostMapping("/create")
     @ResponseBody
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name = SwaggerParameter.Authorization, value = "token", dataType = "String"),
+            @ApiImplicitParam(paramType = "query", name = "username", dataType = "String"),
+            @ApiImplicitParam(paramType = "query", name = "lotteryname", dataType = "String"),
+            @ApiImplicitParam(paramType = "query", name = "lotteryintro", dataType = "String"),
+            @ApiImplicitParam(paramType = "query", name = "timeStart", dataType = "Timestamp"),
+            @ApiImplicitParam(paramType = "query", name = "timeEnd", dataType = "Timestamp"),
+            @ApiImplicitParam(paramType = "query", name = "list<Prizes>", dataType = "Prizes  的一个数组"),
+    })
     public Object createLottery(@RequestBody LotteryService.Create create) {
 
         return lotteryService.createLottery(create);
     }
 
     @PostMapping("/ispass")
-    public Object isPass(long lotteryId,int passcode){
-        return lotteryService.isPass(lotteryId,passcode);
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name =SwaggerParameter.Authorization, value = "抽奖Id", dataType = "String"),
+            @ApiImplicitParam(paramType = "query", name = "lotteryId", value = "抽奖Id", dataType = "long"),
+            @ApiImplicitParam(paramType = "query", name ="passcode", value = "passcode", dataType = "int")
+    })
+    public Object isPass(long lotteryId, int passcode) {
+        return lotteryService.isPass(lotteryId, passcode);
     }
 
 
     @GetMapping("/dolottery")
     @ResponseBody
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name =SwaggerParameter.Authorization, value = "抽奖Id", dataType = "String"),
+            @ApiImplicitParam(paramType = "query", name ="lotteryId", value = "抽奖Id", dataType = "long")
+
+    })
     public Object doLottery(HttpServletRequest request) throws IOException {
 
         return lotteryService.doLottery(request);
 
     }
 
-
     @PostMapping("/prizeList")
-    public Object prizeList(long lotteryId){
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name =SwaggerParameter.Authorization, value = "抽奖Id", dataType = "String"),
+            @ApiImplicitParam(paramType = "query", name ="lotteryId", value = "抽奖Id", dataType = "long")
+    })
+    public Object prizeList(long lotteryId) {
         return lotteryService.message(lotteryId);
     }
 
     @PostMapping("/fuzzy")
-    public Object  findall(int passcode){
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "query", name =SwaggerParameter.Authorization, value = "抽奖Id", dataType = "String"),
+            @ApiImplicitParam(paramType = "query", name ="passcode", value = "passcode", dataType = "long")
+
+    })    public Object findall(int passcode) {
 
         return lotteryService.findbycode(passcode);
     }
