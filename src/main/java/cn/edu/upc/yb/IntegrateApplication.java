bb0c09bc3feabcf607be205d64dd464231f1e280
1409070209 <1142908626@qq.com>
2018-05-06 22:51:36 +0800

    '李良彬你的上传文件配置文件写错了，还是只能上传1M图片'

--- src/main/java/cn/edu/upc/yb/IntegrateApplication.java
+++ src/main/java/cn/edu/upc/yb/IntegrateApplication.java
@@ -2,7 +2,6 @@ package cn.edu.upc.yb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * SpringBoot APP
